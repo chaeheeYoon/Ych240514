ccd48120c87dac178cadfe673307d8920113cd57
ych <korea940414@naver.com>
2024-05-14 09:30:21 +0900

    Player to Goal 이동하는 코드 삽입

--- main.cpp
+++ main.cpp
@@ -4,6 +4,72 @@ using namespace std;
 
 int main()
 {
+	int Screen[10][10]
+	{
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 1, 1, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 1, 0, 0, 2, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+	};
+
+	int PlayerX = 0;
+	int PlayerY = 0;
+	char Key = 0;
+	
+	for (;;)
+	{
+		cin >> Key;
+
+		if (Key == 'w')
+		{
+			PlayerY--;
+		}
+		else if (Key == 's')
+		{
+			PlayerY++;
+		}
+		else if (Key == 'a')
+		{
+			PlayerX--;
+		}
+		else if (Key == 'd')
+		{
+			PlayerX++;
+		}
+
+		system("cls");
+
+		for (int Y = 0; Y < 10; Y++)
+		{
+			for (int X = 0; X < 10; X++)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "P";
+				}
+				else if (Screen[Y][X] == 0)
+				{
+					cout << " ";
+				}
+				else if (Screen[Y][X] == 1)
+				{
+					cout << "*";
+				}
+				else if (Screen[X][Y] == 2)
+				{
+					cout << "G";
+				}
+
+			} cout << endl;
+		}
+
+	}
 
 	return 0;
 }
